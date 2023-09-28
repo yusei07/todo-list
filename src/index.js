e20251cf9b107a9f766fe9b8e2e72fe3326138c8
nora. <usernamesaresoharddtomake@gmail.com>
2023-09-28 23:06:51 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -5,16 +5,13 @@ import {
   timeGreeting,
   dynamicDate,
   displayTaskCount,
-  showInfoModal,
   dynamicModal,
   folderModal
 } from './dom.js';
-import {
-  defaultHTML
-} from './htmlComponents.js'
+import { defaultHTML } from './htmlComponents.js'
 import { ToDo } from './task.js';
 import { Folder } from './folder.js';
-import { checkboxListener } from './handlers.js';
+import { checkboxListener, toDoFeatureHandler, closeModal, getFormDOM } from './handlers.js';
 import { loadHome, loadToday, loadThisWeek, loadImportant, loadCompleted } from './loadTabs';
 
 // globals
@@ -37,7 +34,7 @@ window.addEventListener("load", () => {
   todoAdd(ToDo.tasksArray, "tasks", ToDo.tasksArray);
   folderAdd();
   folderListener();
-  console.log(Folder.folderTasks);
+  // console.log("Folder task:", Folder.folderTasks);
 
   // ui functions
   toggleDarkMode();
@@ -46,6 +43,7 @@ window.addEventListener("load", () => {
   displayTaskCount(ToDo.taskCount);
 
   // page event listeners
+  toDoFeatureHandler(ToDo.tasksArray); // set default todo feature arr
   loadHome(document.querySelector("#todo-container"));
   loadToday(document.querySelector("#todo-container"));
   loadThisWeek(document.querySelector("#todo-container"));
@@ -54,90 +52,11 @@ window.addEventListener("load", () => {
 
   // displayTaskCheck();
 
+  // event listener for checkbox change
+  checkboxListener();
+
   feather.replace();
 })
-
-// todo features handler
-document.addEventListener("click", (e) => {
-  // delete
-  if (e.target.id === "del-btn") {
-    const delTaskIndex = e.target.getAttribute("data-index");
-    // console.log(taskIndex);
-    ToDo.delete(delTaskIndex);
-    localStorage.removeItem(delTaskIndex); // remove checked id (so the next added task wont get checked)
-    displayTaskCount(ToDo.taskCount);
-  }
-
-  // edit
-  if (e.target.id === "edit-btn") {
-    const editTaskIndex = e.target.getAttribute("data-index");
-    const currentEditTask = ToDo.tasksArray[editTaskIndex];
-
-    // assign value to modal
-    const editModalContent = dynamicModal("edit-todo-form", "Edit Task", currentEditTask.title, currentEditTask.description, currentEditTask.dueDate, "Edit");
-
-    const previousModal = document.querySelector("#todo_modal");
-    closeModal(previousModal);
-
-    const editModalContainer = document.createElement("div");
-    editModalContainer.innerHTML = editModalContent;
-    document.body.appendChild(editModalContainer);
-
-    const $todoModal = document.querySelector("#todo_modal");
-    // call daisy ui method (showModal)
-    $todoModal.showModal();
-    listenEditSubmit(currentEditTask);
-  }
-
-  // info
-  if (e.target.id === "info-btn") {
-    // get index
-    const infoTaskIndex = e.target.getAttribute("data-index");
-    const currentTask = ToDo.tasksArray[infoTaskIndex];
-
-    // assign value to modal
-    const infoModalContent = showInfoModal(currentTask.title, currentTask.description, currentTask.dueDate, currentTask.priority);
-
-    // remove existing modal if exists
-    closeModal(document.querySelector("#info_modal"));
-
-    // append the modal content to the body
-    const modalContainer = document.createElement("div");
-    modalContainer.innerHTML = infoModalContent;
-    document.body.appendChild(modalContainer);
-
-    // display modal
-    const $infoModal = document.querySelector("#info_modal");
-    $infoModal.showModal();
-  }
-
-  // if the displayCount is enabled here it will override the taskcount of other taskcount (e.g the completed task)
-  // displayTaskCount(ToDo.taskCount);
-  feather.replace();
-});
-
-// edit submit listener & handler
-function listenEditSubmit(taskTarget) {
-  const editFormDOM = getFormDOM();
-  const $editToDoForm = document.querySelector("#edit-todo-form");
-  // listen for submit
-  $editToDoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    // new value assign to the current task that is being edit
-    const $editTitleInput = $editToDoForm.querySelector("#title");
-
-    taskTarget.title = $editTitleInput.value || taskTarget.title;
-    taskTarget.dueDate = editFormDOM.$date.value || taskTarget.dueDate;
-    taskTarget.description = editFormDOM.$description.value || taskTarget.description;
-    taskTarget.priority = editFormDOM.$priority.value || taskTarget.priority;
-
-    localStorage.setItem("tasks", JSON.stringify(ToDo.tasksArray));
-
-    ToDo.renderToDo("todo-container", ToDo.tasksArray);
-    feather.replace();
-    closeModal(document.querySelector("#todo_modal"));
-  })
-}
 
 // todo btn
 const todoAdd = (array, key, optionalArray) => {
@@ -173,12 +92,11 @@ function listenToDoSubmit(array, key, optionalArray)  {
     const task = new ToDo(addFormDOM.$title.value, addFormDOM.$description.value, addFormDOM.$date.value, addFormDOM.$priority.value);
 
     task.addToDo(array, key, optionalArray); // adds task to desired array
-    // localStorage.setItem(key, JSON.stringify(array))
 
-    ToDo.renderToDo("todo-container", ToDo.tasksArray);
+    ToDo.renderToDo("todo-container", array); // here might be the issue why when i add a task at a folder it renders the home tasks
     feather.replace();
     closeModal(document.querySelector("#todo_modal"));
-    displayTaskCount(ToDo.taskCount);
+    displayTaskCount(array.length); // was originally ToDo.taskCount
   })
 }
 
@@ -255,6 +173,7 @@ const folderListener = () => {
       const folderIndex = e.target.getAttribute("folder-data-index");
       const currentFolderTasks = Folder.folderTasks[folderIndex];
       ToDo.renderToDo("todo-container", currentFolderTasks);
+      toDoFeatureHandler(currentFolderTasks);
 
       todoAdd(currentFolderTasks, "folder-task", Folder.folderTasks);
       // assign the corresponding display task count
@@ -280,26 +199,3 @@ const editFolderSubmit = (folderTarget) => {
     closeModal(document.querySelector("#folder_modal"));
   })
 }
-
-// event listener for checkbox change
-checkboxListener();
-
-const getFormDOM = () => {
-  const $title = document.querySelector("#title");
-  const $description = document.querySelector("#desc");
-  const $date = document.querySelector("#date");
-  const $priority = document.querySelector("#status");
-  return {
-    $title,
-    $description,
-    $date,
-    $priority
-  };
-}
-
-const closeModal = (existingModal) => {
-  if (existingModal) {
-    existingModal.remove();
-  }
-}
-
